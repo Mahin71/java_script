1d5224d9c1164d2569d365dbf6a3a9883a7b5db8
Mahin <mazharulislammahin12@gmail.com>
2021-04-20 21:50:44 +0600

    frist_pro_java_script

---
+++ cleander.js
@@ -0,0 +1,317 @@
+var date = new Date()
+var today = date.getDay()
+var month = date.getMonth()
+var year = date.getFullYear()
+
+
+switch (month){
+    case 0:
+    console.log("january"+year )
+            switch( today ){
+                case 0:
+                    console.log("Sunday")
+                    break
+                case 1:
+                    console.log("Monday")
+                    break
+                case 2:
+                    console.log("Tuesday")
+                    break
+                case 3:
+                    console.log("Wednesday")
+                    break
+                case 4:
+                    console.log("thursday")
+                    break
+                case 5:
+                    console.log("Friday")  
+                    break
+                case 6:
+                    console.log("Saturday")   }
+    break
+    case 1:
+        console.log("February"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+    break
+    case 2:
+        console.log("March "+ year)
+            switch( today ){
+                case 0:
+                    console.log("Sunday")
+                    break
+                case 1:
+                    console.log("Monday")
+                    break
+                case 2:
+                    console.log("Tuesday")
+                    break
+                case 3:
+                    console.log("Wednesday")
+                    break
+                case 4:
+                    console.log("thursday")
+                    break
+                case 5:
+                    console.log("Friday")  
+                    break
+                case 6:
+                    console.log("Saturday")   }
+    break
+    case 3:
+        console.log("April"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 4:
+        console.log("May"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 5:
+        console.log("June"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 6:
+        console.log("July"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 7:
+        console.log("August"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 8:
+        console.log("September"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 9:
+        console.log("October"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 10:
+        console.log("November"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+        case 11:
+        console.log("December"+year)
+        switch( today ){
+            case 0:
+                console.log("Sunday")
+                break
+            case 1:
+                console.log("Monday")
+                break
+            case 2:
+                console.log("Tuesday")
+                break
+            case 3:
+                console.log("Wednesday")
+                break
+            case 4:
+                console.log("thursday")
+                break
+            case 5:
+                console.log("Friday")  
+                break
+            case 6:
+                console.log("Saturday")   }
+        break
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
